cf59dc9ff9afb34417f838ed20d21b54c30eedcd
Laura Lopez <llopezburgos@sapas.com>
2022-06-16 13:39:45 +0000

    solucion al problema del index

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,16 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
+
     function (Controller) {
         "use strict";
 
         return Controller.extend("btptest.invoices.controller.Main", {
             onInit: function () {
                 const oJSONModel = new sap.ui.model.json.JSONModel();
-                oJSONModel.loadData("./model/selectionScreenMenu.json");
+                oJSONModel.loadData("/model/selectionScreenMenu.json");
                 this.getView().setModel(oJSONModel, "selectionScreen");
             }
         });
